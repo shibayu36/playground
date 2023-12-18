ffafc955dfa41d215ea6a06b4363f9c1df7f2817
shibayu36 <shibayu36@gmail.com>
2023-12-18 09:46:35 +0900

    aws-playground

---
+++ aws-playground/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  cloud {
+    organization = "shibayu36"
+    workspaces {
+      name = "aws-playground"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_dynamodb_table" "dynamodb_lock_table" {
+  name         = "lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  #   attribute {
+  #     name = "ExpiredAt"
+  #     type = "S" # RFC3339
+  #   }
+
+  #   attribute {
+  #     name = "ReleaseID"
+  #     type = "S"
+  #   }
+
+  #   attribute {
+  #     name = "Time"
+  #     type = "S" # RFC3339
+  #   }
+}
