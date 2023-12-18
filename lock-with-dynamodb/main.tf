6d7ccafe974073c44bed604a474e91a8a8d057ff
shibayu36 <shibayu36@gmail.com>
2023-12-19 07:05:58 +0900

    Create an user by terraform

--- lock-with-dynamodb/main.tf
+++ lock-with-dynamodb/main.tf
@@ -36,3 +36,45 @@ resource "aws_dynamodb_table" "dynamodb_lock_table" {
   #     type = "S" # RFC3339
   #   }
 }
+
+resource "aws_iam_user" "lock_with_dynamodb_user" {
+  name = "lock-with-dynamodb-user"
+}
+
+resource "aws_iam_access_key" "lock_with_dynamodb_user_key" {
+  user = aws_iam_user.lock_with_dynamodb_user.name
+}
+
+resource "aws_iam_user_policy" "lock_with_dynamodb_user_policy" {
+  name = "lock-with-dynamodb-user-policy"
+  user = aws_iam_user.lock_with_dynamodb_user.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:UpdateItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:Query",
+        "dynamodb:Scan"
+      ],
+      "Resource": "${aws_dynamodb_table.dynamodb_lock_table.arn}"
+    }
+  ]
+}
+EOF
+}
+
+output "access_key" {
+  value = aws_iam_access_key.lock_with_dynamodb_user_key.id
+}
+
+output "secret_key" {
+  sensitive = true
+  value     = aws_iam_access_key.lock_with_dynamodb_user_key.secret
+}
